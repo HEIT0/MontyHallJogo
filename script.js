32ce4addf1dacfd05af5b1c6a11768ac3c0c9411
Heitor Szabo <80275828+HEIT0@users.noreply.github.com>
2021-07-15 22:22:13 +0000

    Mudei o ícone, correção de alguns possíveis erros/desvios, e agr é possível reiniciar sem atualizar a página. Falta escolher a logo definitiva, deixar mais bonito e ver se não há mais erros ou outros possíveis desvios.

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
+var l = document.getElementById('red')
+l.style.visibility = 'hidden'
 var porta0 = document.getElementById('box0')
 var porta1 = document.getElementById('box1')
 var porta2 = document.getElementById('box2')
 var port1 = document.getElementById('port1')
 var port2 = document.getElementById('port2')
 var port3 = document.getElementById('port3')
-var decisao = null
 var tp = document.getElementsByName('red')
-var l = document.getElementById('red')
+var p1 = document.getElementById('p1')
 var pre = Math.floor(Math.random() * 3);
 var premio = pre + 1
 var portas = [1, 2, 3]
+var botbottom = document.getElementById('botaox')
 a = 0
 b = 0
-l.style.visibility = 'hidden'
-
-
+var escolha = 0
+var w = 0
 function clicar0() {
   if (b == 0) {
-    var escolha = 1
+    escolha = 1
     a = escolha
     porta0.style.boxShadow = "inset 0 0 1em gold, 0 0 1em white"
     porta1.style.boxShadow = "3px 3px 10px rgb(0, 0, 0)"
@@ -29,7 +30,7 @@ function clicar0() {
 
 function clicar1() {
   if (b == 0) {
-    var escolha = 2
+    escolha = 2
     a = escolha
     porta0.style.boxShadow = "3px 3px 10px rgb(0, 0, 0)"
     porta1.style.boxShadow = "inset 0 0 1em gold, 0 0 1em white"
@@ -41,7 +42,7 @@ function clicar1() {
 
 function clicar2() {
   if (b == 0) {
-    var escolha = 3
+    escolha = 3
     a = escolha
     porta2.style.boxShadow = "inset 0 0 1em gold, 0 0 1em white"
     porta0.style.boxShadow = "3px 3px 10px rgb(0, 0, 0)"
@@ -51,41 +52,63 @@ function clicar2() {
   }
 }
 var outras = [1, 2, 3]
-function escolha() {
-  var p1 = document.getElementById('p1')
-  p1.innerHTML = `Você escolheu a porta ${a}`
-  b = 1
-  var index = portas.indexOf(a);
-  if (index > -1) {
-    portas.splice(index, 1)
-  }
-  var index2 = portas.indexOf(premio);
-  if (index2 > -1) {
-    portas.splice(index2, 1)
-  }
-  var cabra = aleat()
-  var gol = cabra[0]
-  if (cabra[0] == 1) {
-    port1.src = 'cabra.gif'
-  } else if (cabra[0] == 2) {
-    port2.src = 'cabra.gif'
+function escolher() {
+  if (w == 0) {
+    if (escolha == 0) {
+      alert('Você não selecionou nenhuma porta')
+    } else {
+
+      p1.innerHTML = `Você escolheu a porta ${a}`
+      b = 1
+      var index = portas.indexOf(a);
+      if (index > -1) {
+        portas.splice(index, 1)
+      }
+      var index2 = portas.indexOf(premio);
+      if (index2 > -1) {
+        portas.splice(index2, 1)
+      }
+      var cabra = aleat()
+      var gol = cabra[0]
+      if (cabra[0] == 1) {
+        port1.src = 'cabra.gif'
+      } else if (cabra[0] == 2) {
+        port2.src = 'cabra.gif'
+      } else {
+        port3.src = 'cabra.gif'
+      }
+      outras = [1,2,3]
+      var index = outras.indexOf(a);
+      if (index > -1) {
+        outras.splice(index, 1)
+      }
+      var index2 = outras.indexOf(gol);
+      if (index2 > -1) {
+        outras.splice(index2, 1)
+      }
+
+      p1.innerHTML = `Na porta ${cabra[0]} há cabras, você quer permancer na porta que você escolheu ou mudar para porta ${outras[0]}? `
+      l.style.visibility = 'visible'
+    }
   } else {
-    port3.src = 'cabra.gif'
+    pre = Math.floor(Math.random() * 3);
+    premio = pre + 1
+    portas = [1, 2, 3]
+    a = 0
+    b = 0
+    l.style.visibility = 'hidden'
+    escolha = 0
+    w = 0
+    port1.src = 'porta.png'
+    port2.src = 'porta.png'
+    port3.src = 'porta.png'
+    porta1.style.boxShadow = " 0 0 1em black"
+    porta2.style.boxShadow = " 0 0 1em black"
+    porta0.style.boxShadow = " 0 0 1em black"
+    p1.innerHTML = `Escolha uma das portas para ganhar o prêmio!`
+    botbottom.value = 'Escolha'
   }
-
-  var index = outras.indexOf(a);
-  if (index > -1) {
-    outras.splice(index, 1)
-  }
-  var index2 = outras.indexOf(gol);
-  if (index2 > -1) {
-    outras.splice(index2, 1)
-  }
-
-  p1.innerHTML = `Na porta ${cabra[0]} há cabras, você quer permancer na porta que você escolheu ou mudar para porta ${outras[0]}? `
-  l.style.visibility = 'visible'
 }
-
 function aleat() {
   if (portas.length > 1) {
     let u = portas.length
@@ -165,7 +188,7 @@ function confirm() {
       porta0.style.boxShadow = " 0 0 1em black"
     }
     if (a != premio) {
-      p1.innerHTML = `Parabéns, Você ganhou um DeLorean !`
+      p1.innerHTML = `Parabéns, Você trocou e ganhou um DeLorean !`
       if (premio == 1) {
         port1.src = 'carro.jpg'
         port2.src = 'cabra.gif'
@@ -196,4 +219,6 @@ function confirm() {
       }
     }
   }
+  w = 1
+  botbottom.value = 'Reiniciar'
 }
